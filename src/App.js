960a3e739a5c51857d3c2b2a519099a44bda7db6
CarlosGonzaga11 <carlospzk11@hotmail.com>
2023-03-01 15:54:55 -0300

    transparent-login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <section>
+        <div className="form-box">
+            <div className="form-value">
+            <form action="">
+               <h2>Login</h2>
+               <div className="inputbox">
+                    <EmailOutlinedIcon className="email-icon"/>
+                   <input type="email" required></input>
+                   <label for="">Email</label>
+                </div>
+
+                    <div className="inputbox">
+                    <LockOutlinedIcon className="lock-icon"/>
+                      <input type="password" required></input>
+                      <label for="">Password</label>
+                   </div>
+
+                <div className="forget">
+                  
+                <label for=""><input type="checkbox"></input> Remember me <a href="#">Forgot Password.</a></label>
+       
+               </div>
+          
+              <button>Log in</button>
+              <div className="registercd">
+                <p>Don't have a account <a href="#">Register.</a></p>
+              </div>
+          </form>
+        
+         </div>
+         </div>
+         </section>
+         </div>
+  
   );
 }
 
